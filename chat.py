e5104aaadb6c0822d66243945333be9893e0c4e1
Stephen Collins <stephenc211@gmail.com>
2023-04-14 19:46:15 -0500

    implements first pass sliding conversation context window

--- chat.py
+++ chat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 
+import tiktoken
 import openai
 import os
 import sys
@@ -8,27 +9,60 @@ import datetime
 
 # NOTE: For a more "normal" ChatGPT type of interaction.
 
+MAX_TOKENS = 1500
+COMPLETION_TOKENS = 2500
+MODEL = "gpt-3.5-turbo"
 
 
+encoding = tiktoken.encoding_for_model(MODEL)
 # Set up OpenAI API key and chat log filename
 openai.api_key = os.environ["OPENAI_API_KEY"]
-start_timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+start_timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H')
 chat_log_filename = f"{start_timestamp}_chat_log.txt"
 
 # Create chat_logs directory if it doesn't exist
 if not os.path.exists("logs/chat_logs"):
     os.makedirs("logs/chat_logs")
 
+
 def get_response(messages):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model=MODEL,
         messages=messages,
-        max_tokens=3000,
+        max_tokens=COMPLETION_TOKENS,
         n=1,
         stop="\nUser:",
         temperature=0.7,
     )
     return response.choices[0].message
+
+
+def num_tokens_from_string(string: str) -> int:
+    """Returns the number of tokens in a text string."""
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
+def truncate_messages(messages, chat_log, property, target_sum):
+    current_sum = 0
+    truncated_messages = []
+    next_chat_log = chat_log
+
+    # Loop over the array and sum the "num" property of each dict
+    for message in reversed(messages):
+        current_sum += num_tokens_from_string(message[property])
+
+        # If the sum reaches the target value, break out of the loop and flush the chat_log
+        if current_sum >= target_sum:
+            with open(os.path.join("logs/chat_logs", chat_log_filename), "a") as f:
+                f.write(chat_log)
+                next_chat_log = ''
+            break
+        truncated_messages.insert(0, message)
+
+    # Slice the array to remove the first "num_items" items
+    return truncated_messages, next_chat_log
+
 
 def main():
     chat_log = ""
@@ -45,6 +79,8 @@ def main():
             })
             response = get_response(chat_messages)
             chat_messages.append(response)
+            chat_messages, chat_log = truncate_messages(
+                chat_messages, chat_log, 'content', MAX_TOKENS)
             if response == "":
                 print("Sorry, I didn't understand what you said.")
             elif response == "[STOP]":
@@ -55,13 +91,14 @@ def main():
                 chat_log += f"{timestamp}: User: {user_input}\n"
                 chat_log += f"{timestamp}: AI: {response.content}\n"
                 print(f"AI: {response.content}")
-    except KeyboardInterrupt:
+    except KeyboardInterrupt or Exception:
         with open(os.path.join("logs/chat_logs", chat_log_filename), "a") as f:
             f.write(chat_log)
         sys.exit(1)
 
     with open(os.path.join("logs/chat_logs", chat_log_filename), "a") as f:
         f.write(chat_log)
+
 
 if __name__ == "__main__":
     main()
