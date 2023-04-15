c37c25db310446a96f1bfb71b912c78cba190b0b
Stephen Collins <stephenc211@gmail.com>
2023-04-14 21:21:24 -0500

    implements reusable SlidingContextWindow

--- chat.py
+++ chat.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-
-import tiktoken
 import openai
 import os
 import sys
 import datetime
+from sliding_context_window import SlidingContextWindow
 
 # NOTE: For a more "normal" ChatGPT type of interaction.
 
@@ -13,8 +12,6 @@ MAX_TOKENS = 1500
 COMPLETION_TOKENS = 2500
 MODEL = "gpt-3.5-turbo"
 
-
-encoding = tiktoken.encoding_for_model(MODEL)
 # Set up OpenAI API key and chat log filename
 openai.api_key = os.environ["OPENAI_API_KEY"]
 start_timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H')
@@ -23,7 +20,6 @@ chat_log_filename = f"{start_timestamp}_chat_log.txt"
 # Create chat_logs directory if it doesn't exist
 if not os.path.exists("logs/chat_logs"):
     os.makedirs("logs/chat_logs")
-
 
 def get_response(messages):
     response = openai.ChatCompletion.create(
@@ -36,54 +32,20 @@ def get_response(messages):
     )
     return response.choices[0].message
 
-
-def num_tokens_from_string(string: str) -> int:
-    """Returns the number of tokens in a text string."""
-    num_tokens = len(encoding.encode(string))
-    return num_tokens
-
-
-def truncate_messages(messages, chat_log, property, target_sum):
-    current_sum = 0
-    truncated_messages = []
-    next_chat_log = chat_log
-
-    # Loop over the array and sum the "num" property of each dict
-    for message in reversed(messages):
-        current_sum += num_tokens_from_string(message[property])
-
-        # If the sum reaches the target value, break out of the loop and flush the chat_log
-        if current_sum >= target_sum:
-            with open(os.path.join("logs/chat_logs", chat_log_filename), "a") as f:
-                f.write(chat_log)
-                next_chat_log = ''
-            break
-        truncated_messages.insert(0, message)
-
-    # Slice the array to remove the first "num_items" items
-    return truncated_messages, next_chat_log
-
-
 def main():
     chat_log = ""
-    chat_messages = []
+    context_window = SlidingContextWindow(MAX_TOKENS, MODEL)
     try:
         while True:
             user_input = input("> ")
             if user_input == "quit":
                 break
-            prompt = f"User: {user_input}\nAI:"
-            chat_messages.append({
-                "role": "user",
-                "content": prompt
-            })
-            response = get_response(chat_messages)
-            chat_messages.append(response)
-            chat_messages, chat_log = truncate_messages(
-                chat_messages, chat_log, 'content', MAX_TOKENS)
-            if response == "":
+            context_window.add_message("user", f"User: {user_input}\nAI:")
+            response = get_response(context_window.get_context())
+            context_window.add_message("assistant", response.content)
+            if response.content == "":
                 print("Sorry, I didn't understand what you said.")
-            elif response == "[STOP]":
+            elif response.content == "[STOP]":
                 print("AI: Goodbye!")
                 break
             else:
@@ -96,9 +58,8 @@ def main():
             f.write(chat_log)
         sys.exit(1)
 
-    with open(os.path.join("logs/chat_logs", chat_log_filename), "a") as f:
+    with open(os.path.join("logs/chat_logs", chat_log_filename ), "a") as f:
         f.write(chat_log)
-
 
 if __name__ == "__main__":
     main()
---
+++ sliding_context_window.py
@@ -0,0 +1,41 @@
+from tiktoken import get_encoding
+
+ENCODING = "cl100k_base"
+
+class SlidingContextWindow:
+    def __init__(self, max_tokens, model):
+        self.max_tokens = max_tokens
+        self.model = model
+        self.encoding = get_encoding(ENCODING)
+        self.messages = []
+
+    def num_tokens_from_string(self, string: str) -> int:
+        """Returns the number of tokens in a text string."""
+        num_tokens = len(self.encoding.encode(string))
+        return num_tokens
+
+    def add_message(self, role, content):
+        """Add a new message to the context."""
+        message = {
+            "role": role,
+            "content": content
+        }
+        self.messages.append(message)
+        self.truncate_messages()
+
+    def truncate_messages(self):
+        """Truncate messages to fit within the token limit."""
+        total_tokens = 0
+        truncated_messages = []
+        for message in reversed(self.messages):
+            message_tokens = self.num_tokens_from_string(message["content"])
+            if total_tokens + message_tokens > self.max_tokens:
+                break
+            total_tokens += message_tokens
+            truncated_messages.insert(0, message)
+        self.messages = truncated_messages
+
+    def get_context(self):
+        """Get the current context for the model."""
+        return self.messages
+
